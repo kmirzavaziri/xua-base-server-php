b4e38e662d76a9521e2f0bc65a73801d8bc027c8
kmirzavaziri <k.mirzavaziri@digikala.com>
2021-05-16 21:21:29 +0430

    Fix

--- public_html/Supers/Basics/Highers/File.php
+++ public_html/Supers/Basics/Highers/File.php
@@ -7,6 +7,7 @@ namespace Supers\Basics\Highers;
 
 use ReflectionFunction;
 use Supers\Basics\Boolean;
+use Supers\Basics\Numerics\Integer;
 use Supers\Basics\Strings\Enum;
 use Supers\Basics\Strings\Symbol;
 use Supers\Basics\Strings\Text;
@@ -18,6 +19,12 @@ use XUA\Tools\Signature\SuperArgumentSignature;
 /**
  * @property ?string type
  * @method static SuperArgumentSignature A_type() The Signature of: Argument `type`
+ * @property ?string storeExtension
+ * @method static SuperArgumentSignature A_storeExtension() The Signature of: Argument `storeExtension`
+ * @property bool compress
+ * @method static SuperArgumentSignature A_compress() The Signature of: Argument `compress`
+ * @property ?string maxSize
+ * @method static SuperArgumentSignature A_maxSize() The Signature of: Argument `maxSize`
  * @property bool nullable
  * @method static SuperArgumentSignature A_nullable() The Signature of: Argument `nullable`
  */
@@ -30,6 +37,9 @@ class File extends Super
                 'values' => ['image', 'document', 'archive', 'pdf', 'word', 'text', 'video'],
                 'nullable' => true,
             ]), false, null, false),
+            'storeExtension' => new SuperArgumentSignature(new Text(['nullable' => true]), false, null, false),
+            'compress' => new SuperArgumentSignature(new Boolean([]), false, true, false),
+            'maxSize' => new SuperArgumentSignature(new FileSize(['nullable' => true]), false, null, false),
             'nullable' => new SuperArgumentSignature(new Boolean([]), false, false, false),
         ]);
     }
