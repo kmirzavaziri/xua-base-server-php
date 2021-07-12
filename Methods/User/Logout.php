1273621bb4b7b3eb2904ea1691904e2606b29deb
kmirzavaziri <k.mirzavaziri@digikala.com>
2021-07-12 19:12:40 +0430

    Methods/User/Logout: done.

---
+++ Methods/User/Logout.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Methods\User;
+
+use Services\UserService;
+use Services\XUA\ExpressionService;
+use XUA\Method;
+
+class Logout extends Method
+{
+    protected static function _requestSignatures(): array
+    {
+        return array_merge(parent::_requestSignatures(), [
+        ]);
+    }
+
+    protected static function _responseSignatures(): array
+    {
+        return array_merge(parent::_responseSignatures(), [
+        ]);
+    }
+
+    protected function execute(): void
+    {
+        $session = UserService::session();
+        if (!$session->id) {
+            $this->addAndThrowError('', ExpressionService::get('errormessage.access.denied'));
+        }
+        $session->delete();
+    }
+}
