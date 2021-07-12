3ff0055fbcd551d23a2da988d5eb6fdda4636970
kmirzavaziri <k.mirzavaziri@digikala.com>
2021-07-12 23:44:09 +0430

    Methods/User/GetMinimal: done.

---
+++ Methods/User/GetMinimal.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Methods\User;
+
+use Entities\User;
+use Services\UserService;
+use Services\XUA\Entity\EntityExtractService;
+use Services\XUA\ExpressionService;
+use Supers\Basics\Highers\StructuredMap;
+use XUA\Method;
+use XUA\Tools\Signature\MethodItemSignature;
+
+/**
+ * @property array|object user
+ * @method static MethodItemSignature R_user() The Signature of: Response Item `user`
+ */
+class GetMinimal extends Method
+{
+    protected static function _requestSignatures(): array
+    {
+        return array_merge(parent::_requestSignatures(), [
+        ]);
+    }
+
+    protected static function _responseSignatures(): array
+    {
+        return array_merge(parent::_responseSignatures(), [
+            'user' => new MethodItemSignature(new StructuredMap(['structure' => []]), true, null, false),
+        ]);
+    }
+
+    protected function execute(): void
+    {
+        $user = UserService::user();
+        if (!$user->id) {
+            $this->addAndThrowError('', ExpressionService::get('errormessage.access.denied'));
+        }
+        $this->user = EntityExtractService::fields($user, [User::C_profilePicture(), User::C_firstNameFa(), User::C_lastNameFa()]);
+    }
+}
