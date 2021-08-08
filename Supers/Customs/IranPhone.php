66917a26a6ab6c611a1b711f80731cd52296a52e
kmirzavaziri <k.mirzavaziri@digikala.com>
2021-08-08 14:23:07 +0430

    Added landline phone validation

--- Supers/Customs/IranPhone.php
+++ Supers/Customs/IranPhone.php
@@ -34,8 +34,14 @@ class IranPhone extends Text
 
     protected function _validation(SuperValidationException $exception): void
     {
-        if ($this->type == 'cellphone') {
-            $this->maxLength = 13;
+        switch ($this->type) {
+            case 'cellphone':
+            case 'landline':
+                $this->maxLength = 13;
+                break;
+            case 'fax':
+            default:
+                break;
         }
     }
 
@@ -47,9 +53,31 @@ class IranPhone extends Text
 
         switch ($this->type) {
             case 'cellphone':
+                $validPrefixes = [
+                    '01', '02', '03', '04', '05', '30', '33', '35', '36', '37', '38', '39', '41', /* ایرانسل */
+                    '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', /* همراه اول */
+                    '20', '21', '31', '32', '34' /* سایر */
+                ];
+                $validPrefixesPattern = implode('|', $validPrefixes);
                 $message = ExpressionService::get('errormessage.cellphone.format.is.not.valid');
-                return strlen($input) == 13 and str_starts_with($input, '+989') and !preg_match('/[^0-9+\- ()]/', $input);
+                return preg_match('/^\+989(' . $validPrefixesPattern . ')[0-9]{7}$/', $input);
             case 'landline':
+                $validPrefixes = [
+                    '41', /* آذربایجان شرقی */      '44', /* آذربایجان غربی */ '45', /* اردبیل */
+                    '31', /* اصفهان */              '26', /* البرز */          '84', /* ایلام */
+                    '77', /* بوشهر */               '21', /* تهران */          '56', /* خراسان جنوبی */
+                    '51', /* خراسان رضوی */         '58', /* خراسان شمالی */   '61', /* خوزستان */
+                    '24', /* زنجان */               '23', /* سمنان */          '54', /* سیستان و بلوچستان */
+                    '71', /* فارس */                '28', /* قزوین */          '25', /* قم */
+                    '66', /* لرستان */              '11', /* مازندران */       '86', /* مرکزی */
+                    '76', /* هرمزگان */             '81', /* همدان */          '38', /* چهارمحال و بختیاری */
+                    '87', /* کردستان */             '34', /* کرمان */          '83', /* کرمانشاه */
+                    '74', /* کهگیلویه و بویراحمد */ '17', /* گلستان */         '13', /* گیلان */
+                    '35', /* یزد */
+                ];
+                $validPrefixesPattern = implode('|', $validPrefixes);
+                $message = ExpressionService::get('errormessage.landline.format.is.not.valid');
+                return preg_match('/^\+98(' . $validPrefixesPattern . ')[0-9]{8}$/', $input);
             case 'fax':
             default:
                 $message = ExpressionService::get('errormessage.not.implemented.yet');
@@ -62,7 +90,7 @@ class IranPhone extends Text
         $input = parent::_unmarshal($input);
         return match ($this->type) {
             'cellphone' => strlen($input) < 9 ? $input : '+989' . substr($input, -9),
-            'landline' => $input,
+            'landline' => strlen($input) < 10 ? $input : '+98' . substr($input, -10),
             'fax' => $input,
             default => $input,
         };
