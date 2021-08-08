7f6cd3c9a2cdbdca1ff8f8ef08a9ee45d8189bdc
kmirzavaziri <k.mirzavaziri@digikala.com>
2021-08-08 16:36:19 +0430

    Added Url validation

--- Supers/Customs/Url.php
+++ Supers/Customs/Url.php
@@ -18,14 +18,13 @@ use XUA\Tools\Signature\SuperArgumentSignature;
  */
 class Url extends Text
 {
-    // @TODO add types http/https, mailto, telegram, etc.
     protected function _predicate($input, string &$message = null): bool
     {
         if ($this->nullable and $input === null) {
             return true;
         }
 
-        $message = ExpressionService::get('errormessage.not.implemented.yet');
-        return false;
+        $message = ExpressionService::get('errormessage.url.format.is.not.valid');
+        return filter_var($input, FILTER_VALIDATE_URL);
     }
 }
