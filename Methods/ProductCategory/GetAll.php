c3fa370922ae9260d1a1a059a8349444ebd1d35f
kmirzavaziri <k.mirzavaziri@digikala.com>
2021-08-10 12:46:14 +0430

    Methods/ProductCategory/GetAll: Done.

---
+++ Methods/ProductCategory/GetAll.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Methods\ProductCategory;
+
+use Entities\ProductCategory;
+use XUA\Tools\Signature\EntityFieldSignature;
+use XUA\VARQUE\MethodQuery;
+use XUA\Tools\Signature\MethodItemSignature;
+
+/**
+ * @property array productCategories
+ * @method static MethodItemSignature R_productCategories() The Signature of: Response Item `productCategories`
+ */
+class GetAll extends MethodQuery
+{
+    protected static function entity(): string
+    {
+        return ProductCategory::class;
+    }
+
+    protected static function fields(): array
+    {
+        return [
+            ProductCategory::F_id(),
+            ProductCategory::F_titleFa(),
+            ProductCategory::F_titleEn(),
+        ];
+    }
+
+    protected function feed(): array
+    {
+        return ProductCategory::getMany();
+    }
+
+    protected static function wrapper(): string
+    {
+        return 'productCategories';
+    }
+
+    protected static function association(): ?EntityFieldSignature
+    {
+        return null;
+    }
+}
