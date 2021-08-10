3ebc16681e378d8611e01967d9d43edf2f4fbca9
kmirzavaziri <k.mirzavaziri@digikala.com>
2021-08-10 11:52:34 +0430

    Methods/User/GetJuridical: Done.

---
+++ Methods/User/GetJuridical.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Methods\User;
+
+use Entities\User;
+use Services\UserService;
+use Services\XUA\ExpressionService;
+use XUA\Entity;
+use XUA\Tools\Signature\MethodItemSignature;
+use XUA\VARQUE\MethodView;
+
+
+/**
+ * @property ?string organizationNameFa
+ * @method static MethodItemSignature R_organizationNameFa() The Signature of: Response Item `organizationNameFa`
+ * @property ?string organizationNameEn
+ * @method static MethodItemSignature R_organizationNameEn() The Signature of: Response Item `organizationNameEn`
+ * @property ?string organizationNationalId
+ * @method static MethodItemSignature R_organizationNationalId() The Signature of: Response Item `organizationNationalId`
+ * @property ?string organizationRegistrationId
+ * @method static MethodItemSignature R_organizationRegistrationId() The Signature of: Response Item `organizationRegistrationId`
+ * @property ?string faxNumber
+ * @method static MethodItemSignature R_faxNumber() The Signature of: Response Item `faxNumber`
+ */
+class GetJuridical extends MethodView
+{
+    protected static function entity(): string
+    {
+        return User::class;
+    }
+
+    protected static function fields(): array
+    {
+        return [
+            User::F_organizationNameFa(),
+            User::F_organizationNameEn(),
+            User::F_organizationNationalId(),
+            User::F_organizationRegistrationId(),
+            User::F_faxNumber(),
+        ];
+    }
+
+    protected function feed(): Entity
+    {
+        $user = UserService::user();
+        if (!$user->id) {
+            $this->addAndThrowError('', ExpressionService::get('errormessage.access.denied'));
+        }
+        return $user;
+    }
+}
