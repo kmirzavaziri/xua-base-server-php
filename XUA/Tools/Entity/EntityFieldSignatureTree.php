929559b1f4ede776471a9f45b98949bdefe09086
kmirzavaziri <k.mirzavaziri@digikala.com>
2021-08-19 02:29:04 +0430

    Adding relations to VARQUE: View.

---
+++ XUA/Tools/Entity/EntityFieldSignatureTree.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace XUA\Tools\Entity;
+
+use Exception;
+use Supers\Basics\EntitySupers\EntityRelation;
+use Supers\Basics\Highers\Sequence;
+use Supers\Basics\Highers\StructuredMap;
+use XUA\Entity;
+use XUA\Super;
+use XUA\Tools\Signature\EntityFieldSignature;
+
+final class EntityFieldSignatureTree
+{
+    /**
+     * @var EntityFieldSignatureTree[]
+     */
+    public array $children = [];
+
+    public function __construct(
+        public EntityFieldSignature $value
+    ){}
+
+    public function addChild(EntityFieldSignatureTree|EntityFieldSignature $child): self
+    {
+        if (is_a($child, EntityFieldSignature::class)) {
+            $child = new self($child);
+        }
+
+        if (!is_a($this->value->type, EntityRelation::class)) {
+            throw new Exception("Cannot append children to a non-relational field {$this->value->name}.");
+        }
+
+        if ($this->value->type->relatedEntity != $child->value->entity) {
+            throw new Exception("Cannot append a child from entity {$child->value->entity} to a relational field on entity {$this->value->type->relatedEntity}.");
+        }
+
+        $this->children[] = $child;
+        return $this;
+    }
+
+    public function addChildren(array $children): self
+    {
+        foreach ($children as $child) {
+            $this->addChild($child);
+        }
+        return $this;
+    }
+
+    public function type(): Super
+    {
+        if (is_a($this->value->type, EntityRelation::class)) {
+            if ($this->children) {
+                $structure = [];
+                foreach ($this->children as $child) {
+                    $structure[$child->value->name] = $child->type();
+                }
+                $type = new StructuredMap(['structure' => $structure]);
+            } else {
+                $type = $this->value->type->relatedEntity::F_id()->type;
+            }
+            return $this->value->type->relation[1] == 'N' ? new Sequence(['type' => $type]) : $type;
+        } else {
+            return $this->value->type;
+        }
+    }
+
+    public function value(Entity $entity): mixed
+    {
+        if (is_a($this->value->type, EntityRelation::class)) {
+            if ($this->value->type->relation[1] == 'N') {
+                $return = [];
+                foreach ($entity->{$this->value->name} as $item) {
+                    $return[] = $this->oneItemValue($item);
+                }
+                return $return;
+            } else {
+                return $this->oneItemValue($entity->{$this->value->name});
+            }
+        } else {
+            return $entity->{$this->value->name};
+        }
+    }
+
+    private function oneItemValue(Entity $entity): array|int
+    {
+        if ($this->children) {
+            $return = [];
+            foreach ($this->children as $child) {
+                $return[$child->value->name] = $child->value($entity);
+            }
+            return $return;
+        } else {
+            return $entity->id;
+        }
+    }
+}
--- XUA/VARQUE/MethodView.php
+++ XUA/VARQUE/MethodView.php
@@ -2,8 +2,10 @@
 
 namespace XUA\VARQUE;
 
+use Exception;
 use XUA\Entity;
 use XUA\MethodEve;
+use XUA\Tools\Entity\EntityFieldSignatureTree;
 use XUA\Tools\Signature\EntityFieldSignature;
 use XUA\Tools\Signature\MethodItemSignature;
 
@@ -20,7 +22,13 @@ abstract class MethodView extends MethodEve
         $response = parent::responseSignaturesCalculator();
         $fields = static::fields();
         foreach ($fields as $field) {
-            $response[$field->name] = new MethodItemSignature($field->type, true, null, false);
+            if (is_a($field, EntityFieldSignature::class)) {
+                $response[$field->name] = new MethodItemSignature($field->type, true, null, false);
+            } elseif (is_a($field, EntityFieldSignatureTree::class)) {
+                $response[$field->value->name] = new MethodItemSignature($field->type(), true, null, false);
+            } else {
+                throw new Exception('each field must be an instance of either EntityFieldSignature or EntityFieldSignatureTree');
+            }
         }
         return $response;
     }
@@ -30,16 +38,19 @@ abstract class MethodView extends MethodEve
         $feed = $this->feed();
         $fields = static::fields();
         foreach ($fields as $field) {
-            $this->{$field->name} = $feed->{$field->name};
+            if (is_a($field, EntityFieldSignature::class)) {
+                $this->{$field->name} = $feed->{$field->name};
+            } elseif (is_a($field, EntityFieldSignatureTree::class)) {
+                $this->{$field->value->name} = $field->value($feed);
+            } else {
+                throw new Exception('each field must be an instance of either EntityFieldSignature or EntityFieldSignatureTree');
+            }
         }
     }
 
     # New Overridable Methods
     abstract protected static function entity(): string;
 
-    /**
-     * @return EntityFieldSignature[]
-     */
     abstract protected static function fields(): array;
 
     abstract protected function feed(): Entity;
