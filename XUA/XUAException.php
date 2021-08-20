50fd0ae70415376813593836520746721d95e189
kmirzavaziri <k.mirzavaziri@digikala.com>
2021-08-21 02:23:21 +0430

    added new feature: fromException

--- XUA/XUAException.php
+++ XUA/XUAException.php
@@ -7,6 +7,7 @@ use Exception;
 abstract class XUAException extends Exception
 {
     private array $errors = [];
+
     public function getErrors() : array
     {
         return $this->errors ?: ($this->getMessage() ? ['' => $this->getMessage()] : []);
@@ -21,6 +22,15 @@ abstract class XUAException extends Exception
     public function unsetError(string $key) : XUAException
     {
         unset($this->errors[$key]);
+        return $this;
+    }
+
+    public function fromException(XUAException $exception): XUAException
+    {
+        $errors = $exception->getErrors();
+        foreach ($errors as $key => $message) {
+            $this->setError($key, $message);
+        }
         return $this;
     }
 }
