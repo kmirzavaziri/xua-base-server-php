90a2f0137ddc99ff177cb57588753d7da11246ff
kmirzavaziri <k.mirzavaziri@digikala.com>
2021-08-24 15:40:03 +0430

    Methods/Admin/Product/FieldSignature/GetAllByCategory: Done.

---
+++ Methods/Admin/Product/FieldSignature/GetAllByCategory.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Methods\Admin\Product\FieldSignature;
+
+use Entities\Product\Category;
+use Entities\Product\FieldSignature;
+use Services\UserService;
+use XUA\Tools\Entity\Condition;
+use XUA\Tools\Signature\EntityFieldSignature;
+use XUA\Tools\Signature\VarqueMethodFieldSignature;
+use XUA\VARQUE\MethodQuery;
+use XUA\Tools\Signature\MethodItemSignature;
+
+/**
+ * @property int Q_category
+ * @method static MethodItemSignature Q_category() The Signature of: Request Item `category`
+ * @property array result
+ * @method static MethodItemSignature R_result() The Signature of: Response Item `result`
+ */
+class GetAllByCategory extends MethodQuery
+{
+    protected static function entity(): string
+    {
+        return FieldSignature::class;
+    }
+
+    protected static function _requestSignatures(): array
+    {
+        return array_merge(parent::_requestSignatures(), [
+            'category' => new MethodItemSignature(Category::F_id()->type, true, null, false),
+        ]);
+    }
+
+    protected static function fields(): array
+    {
+        return VarqueMethodFieldSignature::fromList([
+            FieldSignature::F_id(),
+            FieldSignature::F_title(),
+            FieldSignature::F_type(),
+            FieldSignature::F_typeParams(),
+        ]);
+    }
+
+    protected function feed(): array
+    {
+        return FieldSignature::getMany(Condition::leaf(FieldSignature::C_category()->rel(Category::C_id()), Condition::EQ, $this->Q_category));
+    }
+
+    protected static function wrapper(): string
+    {
+        return 'result';
+    }
+
+    protected static function association(): ?EntityFieldSignature
+    {
+        return null;
+    }
+
+    protected function validations(): void
+    {
+        UserService::verifyAdmin($this->error);
+    }
+}
