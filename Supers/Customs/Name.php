9625d08964fd8e30fe06e3f06632ed8e374bd153
kmirzavaziri <k.mirzavaziri@digikala.com>
2021-08-24 17:32:51 +0430

    Added small Hamza.

--- Supers/Customs/Name.php
+++ Supers/Customs/Name.php
@@ -40,7 +40,7 @@ class Name extends Text
         }
 
         $validCharacters = preg_split('//u', match ($this->language) {
-            LocaleLanguage::LANG_FA => 'ابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی ءآأؤئًٌٍَُِّْٰ',
+            LocaleLanguage::LANG_FA => 'ابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی ءٔآأؤئًٌٍَُِّْٰ',
             LocaleLanguage::LANG_EN => 'abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         }, -1, PREG_SPLIT_NO_EMPTY);
         $validCharactersMap = [];
