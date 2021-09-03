436463ade99ae326e5ab1c0ad9aca942e61c7da8
kmirzavaziri <kmirzavaziri@gmail.com>
2021-09-03 05:10:31 +0430

    Methods/Admin/Farm/SetOne: Done.

---
+++ Methods/Admin/Farm/SetOne.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Methods\Admin\Farm;
+
+use Entities\Farm;
+use Entities\Farm\Field;
+use Methods\Abstraction\SetOneByIdAdmin;
+use XUA\Tools\Entity\EntityFieldSignatureTree;
+use XUA\Tools\Signature\VarqueMethodFieldSignature;
+
+class SetOne extends SetOneByIdAdmin
+{
+    protected static function entity(): string
+    {
+        return Farm::class;
+    }
+
+    protected static function fields(): array
+    {
+        return VarqueMethodFieldSignature::fromList([
+            Farm::F_title(),
+            Farm::F_description(),
+            Farm::F_story(),
+            (new EntityFieldSignatureTree(Farm::F_additionalFields()))->addChildren([
+                Field::F_id(),
+                Field::F_fieldSignature(),
+                Field::F_value(),
+            ]),
+            Farm::F_averageAnnualInterest(),
+            Farm::F_status(),
+            Farm::F_owner(),
+            Farm::F_products(),
+            Farm::F_geographicDivision(),
+            Farm::F_address(),
+            Farm::F_geolocationLat(),
+            Farm::F_geolocationLong(),
+            Farm::F_cooperationField(),
+        ], false);
+    }
+}
