f41239cb47b7f5f69c678689dd09253fd4f00214
kmirzavaziri <kmirzavaziri@gmail.com>
2021-09-04 14:59:13 +0430

    better

--- Services/XUA/EnvironmentService.php
+++ Services/XUA/EnvironmentService.php
@@ -9,15 +9,11 @@ use XUA\Service;
 abstract class EnvironmentService extends Service
 {
     const ENV_LOCAL = 'local';
+    const ENV_DEMO = 'prod';
     const ENV_PROD = 'prod';
 
     public static function env(): string
     {
         return getenv('ENV_NAME');
-    }
-
-    public static function isProd(): bool
-    {
-        return static::env() == static::ENV_PROD;
     }
 }
