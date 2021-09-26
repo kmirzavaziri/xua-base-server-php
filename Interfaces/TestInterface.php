8b9fba118c4d3f8d6e8e937bb96c335d3a7b4409
kmirzavaziri <kmirzavaziri@gmail.com>
2021-09-26 23:08:18 +0330

    Added item code generation service, and temporarily modified the test interface.

--- Interfaces/TestInterface.php
+++ Interfaces/TestInterface.php
@@ -2,13 +2,19 @@
 
 namespace Interfaces;
 
+use Entities\Item;
+use Entities\Product;
 use Services\EnvironmentService;
+use Services\ItemService;
 use XUA\InterfaceEve;
 
 class TestInterface extends InterfaceEve
 {
     public static function execute(): string
     {
+        $item = new Item(3);
+        $code = ItemService::generateCode($item);
+        var_dump($code);
         return 'Hello World! This is the ' . EnvironmentService::getEnv() . ' environment.';
     }
 }
