1ddd7796131b907642ecf349af41b89d89e22686
Kamyar Mirzavaziri <kmirzavaziri@gmail.com>
2021-11-17 18:30:56 +0330

    moved dev credentials to xua-config

--- src/Services/Dev/Credentials.php
+++ src/Services/Dev/Credentials.php
@@ -2,16 +2,13 @@
 
 namespace Xua\Core\Services\Dev;
 
+use Xua\Core\Services\ConstantService;
 use Xua\Core\Services\ExpressionService;
 use Xua\Core\Exceptions\MethodRequestException;
 use Xua\Core\Eves\Service;
 
 final class Credentials extends Service
 {
-    private const DEVS = [
-        'kamyar' => '$2y$10$Q0ESdY25efJwj4Yd4H.Lw.sy2x.qRD5MGsh/v5y3/ZOT/5GLe8MJS',
-    ];
-
     private static ?string $developer = null;
 
     public static function developer(): ?string
@@ -25,9 +22,10 @@ final class Credentials extends Service
     {
         if (isset($_SERVER['HTTP_XUA_DEV_CREDENTIALS'])){
             $headerStringValue = explode(':', $_SERVER['HTTP_XUA_DEV_CREDENTIALS']);
+            $devs = ConstantService::get('xua', 'services.sec.devs');
             if (count($headerStringValue) == 2
-                and isset(Credentials::DEVS[$headerStringValue[0]])
-                and password_verify($headerStringValue[1], Credentials::DEVS[$headerStringValue[0]])
+                and isset($devs[$headerStringValue[0]])
+                and password_verify($headerStringValue[1], $devs[$headerStringValue[0]])
             ) {
                 self::$developer = $headerStringValue[0];
             }
