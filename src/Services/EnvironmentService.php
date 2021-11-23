04ba63814e513db8138eade8679face64ba865dc
Kamyar Mirzavaziri <kmirzavaziri@gmail.com>
2021-11-23 09:15:25 +0330

    activated twig dump on debug environments

--- src/Services/EnvironmentService.php
+++ src/Services/EnvironmentService.php
@@ -7,11 +7,17 @@ use Xua\Core\Eves\Service;
 abstract class EnvironmentService extends Service
 {
     const ENV_LOCAL = 'local';
-    const ENV_DEMO = 'prod';
+    const ENV_DEMO = 'demo';
     const ENV_PROD = 'prod';
 
     public static function env(): string
     {
         return getenv('ENV_NAME');
     }
+
+    public static function debugMode(): bool
+    {
+        return self::env() != self::ENV_PROD;
+    }
+
 }
--- src/Services/TemplateService.php
+++ src/Services/TemplateService.php
@@ -3,20 +3,25 @@
 namespace Xua\Core\Services;
 
 use Twig\Environment;
+use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
 use Xua\Core\Eves\Service;
 
 final class TemplateService extends Service
 {
-    private static Environment $twig;
+    public static Environment $twig;
 
     private function __construct() {}
 
     protected static function _init(): void
     {
         self::$twig = new Environment(new FilesystemLoader(ConstantService::get('config', 'services.template.path')), [
+            'debug' => EnvironmentService::debugMode(),
             'cache' => ConstantService::get('config', 'services.template.cachePath'),
         ]);
+        if (EnvironmentService::debugMode()) {
+            self::$twig->addExtension(new DebugExtension());
+        }
     }
 
     public static function render(string $template, array $bind) : string
