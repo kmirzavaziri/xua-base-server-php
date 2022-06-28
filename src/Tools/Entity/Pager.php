347ab316f41d54dd0470611d59e499f28d0ce669
Kamyar Mirzavaziri <kmirzavaziri@gmail.com>
2022-06-28 12:20:50 +0430

    added readonly modifier to pageSize in Pager class (new feature of PHP)

--- src/Tools/Entity/Pager.php
+++ src/Tools/Entity/Pager.php
@@ -5,8 +5,8 @@ namespace Xua\Core\Tools\Entity;
 class Pager
 {
     public function __construct(
-        private int $pageSize,
-        private int $pageIndex
+        private readonly int $pageSize,
+        private          int $pageIndex
     ) {}
 
     public static function unlimited() : Pager
