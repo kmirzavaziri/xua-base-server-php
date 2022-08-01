a3c73312d60ff25f7c98a9d7b3bfa4879f7c7090
Kamyar Mirzavaziri <kmirzavaziri@gmail.com>
2022-08-01 22:09:25 +0430

    better to read DISTINCT when selecting from db

--- src/Eves/Entity.php
+++ src/Eves/Entity.php
@@ -834,7 +834,7 @@ abstract class Entity extends Block
     final protected static function _x_getMany(Condition $condition, Order $order, Pager $pager, string $lock): array
     {
         [$columnsExpression, $keys] = self::columnsExpression();
-        $statement = self::execute("SELECT $columnsExpression FROM `" . static::table() . "` " . $condition->joins() . " WHERE $condition->template " . $order->render() . $pager->render() . " " . $lock, $condition->parameters);
+        $statement = self::execute("SELECT DISTINCT $columnsExpression FROM `" . static::table() . "` " . $condition->joins() . " WHERE $condition->template " . $order->render() . $pager->render() . " " . $lock, $condition->parameters);
         $rawArrays = $statement->fetchAll(PDO::FETCH_NUM);
         $arrays = [];
         foreach ($rawArrays as $item => $rawArray) {
